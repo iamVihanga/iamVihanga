950733733cb6701261262cf57a438d9c29895eeb
iamVihanga <vihangarashansilva@gmail.com>
2026-02-09 15:29:09 +0530

    major: remove all nextjs project files for 2026 upgrade

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["skillicons.dev", "lgrjpckdqgtibclhjdhp.supabase.co"],
-    dangerouslyAllowSVG: true,
-  },
-};
-
-export default nextConfig;
